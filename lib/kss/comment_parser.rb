04a38c36ced9584520c043142edb2ed5e12b3649
Kyle Neath <kneath@gmail.com>
2012-02-06 11:54:10 -0800

    parse single-line and multi-line comments

--- lib/kss/comment_parser.rb
+++ lib/kss/comment_parser.rb
@@ -1,14 +1,40 @@
 module Kss
-  # Takes a file path of a text file and extracts comments from it. Currently
-  # accepts two formats:
+  # Public: Takes a file path of a text file and extracts comments from it.
+  # Currently accepts two formats:
   #
   # // Single line style.
   # /* Multi-line style. */
   class CommentParser
 
-    attr_reader :blocks
+    # Returns a boolean.
+    def self.single_line_comment?(line)
+      !!(line =~ /[\s+]?\/\//)
+    end
 
-    # Initializes a new comment parser object. Does not parse on
+    # Returns a boolean.
+    def self.start_multi_line_comment?(line)
+      !!(line =~ /[\s+]?\/\*/)
+    end
+
+    # Returns a boolean.
+    def self.end_multi_line_comment?(line)
+      !!(line =~ /[.+]?\*\//)
+    end
+
+    # Returns a String.
+    def self.parse_single_line(line)
+      matches = line.match /([.+]?\/\/)(.+)?/
+      matches[2].to_s
+    end
+
+    # Returns a String.
+    def self.parse_multi_line(line)
+      cleaned = line.to_s.sub(/\/\*/, '')
+      cleaned = cleaned.sub(/\*\//, '')
+      cleaned
+    end
+
+    # Public: Initializes a new comment parser object. Does not parse on
     # initialization.
     #
     # file_path - The location of the file to parse as a String.
@@ -17,8 +43,78 @@ module Kss
     #                          markers (default:true).
     #
     def initialize(file_path, options={})
-      options[:preserve_whitespace] = true if options[:preserve_whitespace].nil?
+      @options = options
+      @options[:preserve_whitespace] = true if @options[:preserve_whitespace].nil?
+      @file_path = file_path
       @blocks = []
+      @parsed = false
+    end
+
+    # Public: The different sections of parsed comment text. A section is
+    # either a multi-line comment block's content, or consecutive lines of
+    # single-line comments.
+    #
+    # Returns an Array of parsed comment Strings.
+    def blocks
+      @parsed ? @blocks : parse_blocks
+    end
+
+
+    def parse_blocks
+      File.open @file_path do |file|
+        current_block = nil
+        inside_single_line_block = false
+        inside_multi_line_block  = false
+
+        file.each_line do |line|
+          # Parse single-line style
+          if self.class.single_line_comment?(line)
+            parsed = self.class.parse_single_line line
+            if inside_single_line_block
+              current_block += "\n#{parsed}"
+            else
+              current_block = parsed.to_s
+              inside_single_line_block = true
+            end
+          end
+
+          # Parse multi-lines tyle
+          if self.class.start_multi_line_comment?(line) || inside_multi_line_block
+            parsed = self.class.parse_multi_line line
+            if inside_multi_line_block
+              current_block += "\n#{parsed}"
+            else
+              current_block = parsed
+              inside_multi_line_block = true
+            end
+          end
+
+          # End a multi-line block if detected
+          inside_multi_line_block = false if self.class.end_multi_line_comment?(line)
+
+          if !self.class.single_line_comment?(line) && !inside_multi_line_block
+            # Store the current comment block
+            @blocks << normalize(current_block) if inside_single_line_block || inside_multi_line_block
+
+            inside_single_line_block = false
+            inside_multi_line_block  = false
+            current_block = nil
+          end
+        end
+      end
+
+      @parsed = true
+      @blocks
+    end
+
+    # Normalizes the comment block to ignore any consistent preceding
+    # whitespace. Consistent means the same amount of whitespace on every line
+    # of the comment block.
+    #
+    # Returns a String of normalized text.
+    def normalize(text_block)
+      return text_block if @options[:preserve_whitespace]
+      text_block
     end
 
   end
--- test/comment_parser_test.rb
+++ test/comment_parser_test.rb
@@ -7,6 +7,29 @@ class CommentParser < Kss::Test
     @parsed_comments = Kss::CommentParser.new(loc).blocks
   end
 
+  test "detects single-line comment syntax" do
+    assert Kss::CommentParser.single_line_comment?("// yuuuuup")
+    assert !Kss::CommentParser.single_line_comment?("nooooope")
+  end
+
+  test "detects start of multi--line comment syntax" do
+    assert Kss::CommentParser.start_multi_line_comment?("/* yuuuuup")
+    assert !Kss::CommentParser.start_multi_line_comment?("nooooope")
+  end
+
+  test "detects end of multi-line comment syntax" do
+    assert Kss::CommentParser.end_multi_line_comment?(" yuuuuup */")
+    assert !Kss::CommentParser.end_multi_line_comment?("nooooope")
+  end
+
+  test "parses the single-line comment syntax" do
+    assert_equal " yuuuuup", Kss::CommentParser.parse_single_line("// yuuuuup")
+  end
+
+  test "parses the multi-line comment syntax" do
+    assert_equal " yuuuup ", Kss::CommentParser.parse_multi_line("/* yuuuup */")
+  end
+
   test "finds single-line comment styles" do
     expected  = <<comment
 This comment block has comment identifiers on every line.
