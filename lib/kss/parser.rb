f3f4950a95c167e5530887ee850b5a0339fb4290
johny <johny@johnys-MacBook-Air.local>
2013-12-13 14:39:05 -0500

    Add parenthesis for consistancy

--- lib/kss/parser.rb
+++ lib/kss/parser.rb
@@ -30,13 +30,13 @@ module Kss
           kss_string = path_or_string
           parser = CommentParser.new(kss_string)
           parser.blocks.each do |comment_block|
-            add_section comment_block if self.class.kss_block?(comment_block)
+            add_section(comment_block) if self.class.kss_block?(comment_block)
           end
         end
       end
     end
 
-    def add_section comment_text, filename = ''
+    def add_section(comment_text, filename = '')
       base_name = File.basename(filename)
       section = Section.new(comment_text, base_name)
       @sections[section.section] = section
